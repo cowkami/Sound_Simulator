8eb13af82885460146c5af3444c83c2090a89137
lee <lee.unseo17@gmail.com>
2019-10-09 03:21:11 +0900

    PML by Chu Ma

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# from time import sleep
 from backend import backend as bd
 from backend import set_backend
 from matrix_visualizer import MatrixVisualizer
 
-set_backend('torch.cuda')
+set_backend('torch.cuda.float32')
 
 NX = 500
 NY = 300
@@ -16,37 +15,64 @@ N_STEP = 10000
 
 freq = 1.0e+03
 
-rho = 13
-kappa = 142.0e3
+C = 34000
+rho = 1.2
+kappa = C * rho
 
+
+# PML layer
+S_max = 50  # max sigma means maximum attenuation coefficients.
+W_PML = 20  # width of perfetly matched layer.
+Sx = bd.zeros((NX+1, NY,   NZ))
+Sy = bd.zeros((NX,   NY+1, NZ))
+Sz = bd.zeros((NX,   NY,   NZ+1))
+
+# set sigma which is increase from the inner boundary
+# to the outer of the PML.
+S_inc = (dx * bd.arange(0, W_PML, 1.0) / W_PML) * S_max
+
+for i, sigma in enumerate(S_inc, 1):
+    Sx[W_PML - i, :, :] = sigma
+    Sx[-W_PML + i, :, :] = sigma
+    Sy[:, W_PML - i, :] = sigma
+    Sy[:, -W_PML + i, :] = sigma
+    Sz[:, :, W_PML - i] = sigma
+    Sz[:, :, -W_PML + i] = sigma
+
+# set tensor
 Vx = bd.zeros((NX+1, NY,   NZ))
 Vy = bd.zeros((NX,   NY+1, NZ))
 Vz = bd.zeros((NX,   NY,   NZ+1))
 P = bd.zeros((NX,   NY,   NZ))
 
-vis = MatrixVisualizer(1920, int(NY*1920/(NY+NZ)))
+vis = MatrixVisualizer(4*NX, 4*(NY+NZ))
 
-max_ = 0
 for n in range(N_STEP+1):
-    Vx[1:-1, :, :] -= (dt / (rho * dx)) * (P[1:, :, :] - P[:-1, :, :])
-    Vy[:, 1:-1, :] -= (dt / (rho * dx)) * (P[:, 1:, :] - P[:, :-1, :])
-    Vz[:, :, 1:-1] -= (dt / (rho * dx)) * (P[:, :, 1:] - P[:, :, :-1])
-    P[:NX, :NY, :NZ] -= (kappa * dt / dx) * (
-        (Vx[1:, :, :] - Vx[:-1, :, :]) +
-        (Vy[:, 1:, :] - Vy[:, :-1, :]) +
-        (Vz[:, :, 1:] - Vz[:, :, :-1])
-    )
+    Vx[1:-1, :, :] -= (dt / (rho * dx)) * (P[1:, :, :] - P[:-1, :, :]) \
+        + (C * dt) * Sx[1:-1, :, :] * Vx[1:-1, :, :]
+    Vy[:, 1:-1, :] -= (dt / (rho * dx)) * (P[:, 1:, :] - P[:, :-1, :]) \
+        + (C * dt) * Sy[:, 1:-1, :] * Vy[:, 1:-1, :]
+    Vz[:, :, 1:-1] -= (dt / (rho * dx)) * (P[:, :, 1:] - P[:, :, :-1]) \
+        + (C * dt) * Sz[:, :, 1:-1] * Vz[:, :, 1:-1]
+
+    P -= (kappa * dt / dx) \
+        * (   (Vx[1:, :, :] - Vx[:-1, :, :])
+            + (Vy[:, 1:, :] - Vy[:, :-1, :])
+            + (Vz[:, :, 1:] - Vz[:, :, :-1])
+            + Sx[1:, :, :] * (dt / dx) * (Vx[1:, :, :] - Vx[:-1, :, :])
+            + Sy[:, 1:, :] * (dt / dx) * (Vy[:, 1:, :] - Vy[:, :-1, :])
+            + Sz[:, :, 1:] * (dt / dx) * (Vz[:, :, 1:] - Vz[:, :, :-1])) \
+        + (C * dt) * (Sx[1:, :, :] + Sy[:, 1:, :] + Sz[:, :, 1:]) * P
+
+    # input signal
     if n % 500 > 0 and n % 500 < 50:
         amp = 10
         theta = bd.array(2.0 * bd.pi * freq * n * dt)
-        sig = amp * ((1.0 - bd.cos(theta)) / 2.0) * bd.sin(theta)
-    else:
-        sig = 0.0
-
-    P[int(NX/4), int(NY*4/11):int(NY*6/11), int(NZ/3):int(NZ*2/3)] = sig
+        sig = amp * bd.sin(theta)
+        P[int(NX/4), int(NY*4/11):int(NY*6/11), int(NZ/3):int(NZ*2/3)] = sig
 
     # obstacle
     P[int(NX/2):int(NX*2/3), int(NY/3):int(NY*2/3), :] *= 0.2
 
+    # visualise update
     vis.update(bd.numpy(bd.cat((P[:, :, int(NZ/2)], P[:, int(NY/3), :]), axis=1)))
-#    sleep(.005)
