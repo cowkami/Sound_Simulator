48ebd6fc333f975ec66e15189c6027a47a6e669d
lee <lee.unseo17@gmail.com>
2019-10-21 15:25:17 +0900

    modify main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ NX = 500
 NY = 300
 NZ = 100
 
-dx = 0.01
+dx = 0.001
 dt = 20.0e-6
 
 N_STEP = 10000
@@ -21,7 +21,7 @@ kappa = C * rho
 
 
 # PML layer
-S_max = 50  # max sigma means maximum attenuation coefficients.
+S_max = 10000  # max sigma means maximum attenuation coefficients.
 W_PML = 30  # width of perfetly matched layer.
 Sx = bd.zeros((NX+1, NY,   NZ))
 Sy = bd.zeros((NX,   NY+1, NZ))
@@ -79,15 +79,15 @@ for n in range(N_STEP+1):
             + Sz[:, :, 1:]
         ) * P
 
-    # input signal
+    # sound source
     if n % 500 > 0 and n % 500 < 50:
-        amp = 5
+        amp = 0.0000001
         theta = bd.array(2.0 * bd.pi * freq * n * dt)
         sig = amp * bd.sin(theta)
-        P[int(NX/4), int(NY*4/11):int(NY*6/11), int(NZ/3):int(NZ*2/3)] = sig
+        P[int(NX/4), int(NY*4/11):int(NY*6/11), int(NZ/3):int(NZ*2/3)] += ((kappa * dt) / (dx**3)) * sig
 
     # obstacle
-    P[int(NX/2):int(NX*2/3), int(NY/3):int(NY*2/3), :] *= 0.2
+    P[int(NX/2):int(NX*2/3), int(NY/3):int(NY*2/3), :] = 0
 
     # visualise update
     vis.update(bd.numpy(bd.cat((P[:, :, int(NZ/2)], P[:, int(NY/3), :]), axis=1)))
